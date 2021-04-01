3724da7a18810795d1f7f6193ee5044ea94b8461
Filipe Simon <filipesimon@live.com>
2021-04-01 01:37:39 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,33 +1,80 @@
-window.onload = function() {
-  nextStep();
-}
+// window.onload = function() {
+//   nextStep();
+// }
 
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(
-      function(){
-        resolve(true)
-      },
-      ms
-    );
+// function sleep(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(
+//       function(){
+//         resolve(true)
+//       },
+//       ms
+//     );
+//   });
+// }
+
+// function colorStep(sleepResponse, step) {
+//   if (sleepResponse) {
+//     return new Promise((resolve) => {
+//       step.classList.add("active");
+//       resolve();
+//     });
+//   }
+// }
+
+// async function nextStep() {
+//   var numberOfSteps = document.getElementsByClassName('progressbar')[0].getElementsByTagName('li').length;
+//   var steps = document.getElementsByClassName('progressbar')[0].getElementsByTagName('li');
+    
+//   for (i = 0; i < numberOfSteps; i++) {
+//     let sleepResponse = await sleep(1000);
+//     let colorStepResponse = await colorStep(sleepResponse, steps[i]);
+//   }
+// }
+
+var activeStep = 1;
+
+const executeStep = () => {
+  return new Promise(async(resolve) => {
+    if (activeStep == 1) {
+      let stepOneResponse = await stepOne();
+      resolve(stepOneResponse);
+    } else if (activeStep == 2) {
+      let stepTwoResponse = await stepTwo();
+      resolve(stepTwoResponse);
+    }
   });
 }
 
-function colorStep(sleepResponse, step) {
-  if (sleepResponse) {
-    return new Promise((resolve) => {
-      step.classList.add("active");
-      resolve();
-    });
-  }
+const stepOne = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+        status = 'active';
+        resolve(status);
+    }, 2000);
+  });
 }
 
-async function nextStep() {
-  var numberOfSteps = document.getElementsByClassName('progressbar')[0].getElementsByTagName('li').length;
-  var steps = document.getElementsByClassName('progressbar')[0].getElementsByTagName('li');
-    
-  for (i = 0; i < numberOfSteps; i++) {
-    let sleepResponse = await sleep(1000);
-    let colorStepResponse = await colorStep(sleepResponse, steps[i]);
-  }
+const stepTwo = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+        status = 'active';
+        resolve(status);
+    }, 5000);
+  });
 }
+
+const nextStep = (status) => {
+  if (status == 'active') {
+    document.querySelector(`[step="${activeStep}"]`).classList.add(status);
+    activeStep++;
+  }
+
+  return true;
+}
+
+$('#btn-operacional-message').click(async () => {
+  let executeStepResponse = await executeStep();
+  console.log(executeStepResponse);
+  let nextStepResponse = await nextStep(executeStepResponse);
+});
